ca5dbdcd245fcce019e221bd83ee03537e33e7e0
c9dong <davidCdong_123@hotmail.com>
2016-10-29 15:27:06 -0400

    map generation

---
+++ Assets/Scripts/MapController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MapController : MonoBehaviour {
+	public int rows = 10;
+	public int columns = 10;
+
+	public GameObject floorTile;
+	public GameObject player;
+
+	private Transform boardHolder;
+	private List<Vector3> gridPositions = new List<Vector3>();
+
+	void initList() {
+		gridPositions.Clear ();
+		for (int i=0; i<rows; i++) {
+			for (int j=0; j<columns; j++) {
+				gridPositions.Add(new Vector3(i,j,0f));
+			}
+		}
+	}
+
+	void setupFloor() {
+		boardHolder = GameObject.Find ("Map").transform;
+		for (int x = -columns/2; x < columns/2; x++) {
+			GameObject instance1 = Instantiate (floorTile, new Vector3 (x, -rows/2, 0f), Quaternion.identity) as GameObject;
+			GameObject instance2 = Instantiate (floorTile, new Vector3 (x, rows/2, 0f), Quaternion.identity) as GameObject;
+
+			instance1.transform.SetParent (boardHolder);
+			instance2.transform.SetParent (boardHolder);
+		}
+	}
+
+	public void initPlayer() {
+		Instantiate (player, new Vector3 (0, 0, 0), Quaternion.identity);
+	}
+
+	public void setupScene() {
+		setupFloor ();
+		initPlayer ();
+	}
+}
